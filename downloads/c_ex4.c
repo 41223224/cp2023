cfbcc7b98219f40a11d8a88946e23f7d6b83b610
41223224 <144990214+41223224@users.noreply.github.com>
2024-01-04 13:08:30 +0000

    新增c_ex4,c_ex5 練習四,五

---
+++ downloads/c_ex4.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+  char x,y;
+  x='a';
+  y=(char)97;
+  printf("x=%c,y=%c,ASCII of y=%d",x,y,y);
+  return 0;
+}
---
+++ downloads/c_ex5.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+  int a,b;
+  float c;
+  a=b=5+1;
+  c=(float)a+0.1;
+  printf("%.1f",c);
+}
