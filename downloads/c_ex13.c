25f51effaaebb61fb4b0e78c1ccb0f9896301cb2
41223224 <144990214+41223224@users.noreply.github.com>
2024-01-09 09:18:25 +0000

    新增c_ex13第十三題

---
+++ downloads/c_ex13.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    float this_is_a_number1,this_is_a_number2,total;
+    printf("Please enter an number:\n");
+        scanf("%f",&this_is_a_number1);
+        printf("You entered %f\n",this_is_a_number1);
+        printf("Please enter another number:\n");
+        scanf("%f",&this_is_a_number2);
+        printf("You entered %f\n",this_is_a_number2);
+        total=this_is_a_number1+this_is_a_number2;
+        printf("total is %f\n",total);
+        return 0;
+
+}
