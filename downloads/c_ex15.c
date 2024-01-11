3bb910768346770d070b79c610e729779ed60b36
41223224 <144990214+41223224@users.noreply.github.com>
2024-01-11 05:18:55 +0000

    新增c_ex15第十五題

---
+++ downloads/c_ex15.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+float this_is_a_number, total;
+int i;
+total = 0;
+i = 0;
+do {
+printf("Please enter a number:\n ");
+scanf("%f", &this_is_a_number);
+total = total + this_is_a_number;
+i++;
+}while( i < 10);
+printf("Total Sum is = %f\n", total);
+}
